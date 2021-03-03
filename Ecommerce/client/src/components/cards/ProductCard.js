816dd7de5d7737959c273ac7008407a177e36961
Danh Nguyen <vn1007347@gmail.com>
2021-03-02 22:35:45 -0800

    Rating System and Related Places

--- Ecommerce/client/src/components/cards/ProductCard.js
+++ Ecommerce/client/src/components/cards/ProductCard.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {Card} from 'antd';
 import {DeleteOutlined, EditOutlined, EyeOutlined, ShoppingCartOutlined} from "@ant-design/icons";
 import Default from '../../images/Default.jpg';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import {showAverage} from "../../functions/rating";
+
 
 const {Meta} = Card;
 
@@ -11,7 +13,10 @@ const ProductCard = ({product}) => {
     //destructure
     const {images, title, description, slug} = product;
     return(
-
+    <>
+        {product && product.ratings && product.ratings.length > 0
+            ? showAverage(product)
+            : <div className="text-center pt-1 pb-3">No rating yet</div>}
     <Card
         cover={
         <img
@@ -35,6 +40,7 @@ const ProductCard = ({product}) => {
             description={`${description && description.substring(0, 50)}...`}
         />
     </Card>
+        </>
 )};
 
 export default ProductCard;
---
+++ Ecommerce/client/src/functions/rating.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import StarRating from 'react-star-ratings'
+
+
+export const showAverage = (p) => {
+    if(p && p.ratings){
+        let ratingsArray = p && p.ratings
+        let total = []
+        let length = ratingsArray.length
+
+        ratingsArray.map((r) => total.push(r.star))
+        let totalReduced = total.reduce((prev, next) => prev + next, 0) // 0 is initial value
+        console.log('totalReduced', totalReduced)
+        let highest = length * 5;
+        console.log('highest', highest)
+        let result = (totalReduced * 5) / highest;
+        console.log('result', result);
+
+        return (
+            <div className="text-center pt-1 pb-3">
+                <span>
+                    <StarRating
+                        starDimension="20px"
+                        starSpacing="2px"
+                        starRatedColor="red"
+                        rating={result}
+                        editing={false}
+                    />
+                    {" "}({p.ratings.length})
+                </span>
+            </div>
+        )
+    }
+};
