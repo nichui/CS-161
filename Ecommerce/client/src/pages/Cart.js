40166c6baeb26e4d42f44078f15d60d41158423c
Danh Nguyen <vn1007347@gmail.com>
2021-03-04 20:17:38 -0800

    Created Cart page and Calculated Total

---
+++ Ecommerce/client/src/pages/Cart.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import {useSelector, useDispatch} from "react-redux";
+import {Link} from 'react-router-dom'
+
+const Cart = () => {
+    const {cart, user} = useSelector((state) => ({...state}));
+    const dispatch = useDispatch();
+
+    // [1,2] 100 + 200 = 300
+    const getTotal = () => {
+
+            return cart.reduce((currentValue, nextValue) => {
+                return currentValue + nextValue.count * nextValue.price;
+            },0)}
+
+    
+    return(
+        <div className="container-fluid pt-2">
+
+
+            <div className="row">
+                <div className="col-md-8">
+                    <h4> Cart / {cart.length} Product</h4>
+                    {!cart.length ?
+                        (<h4>No products in cart. <Link to="/shop">Continue Shopping.</Link></h4>)
+                        :
+                        ('show cart items')}
+                </div>
+
+                <div className="col-md-4">
+                    <h4>Order Summary</h4>
+                    <hr/>
+                    <p>Products</p>
+                    {cart.map((c,i) => (
+                        <div key={i}>
+                            <p>
+                                <p>{c.title} x {c.count} ticket(s) = ${c.price * c.count}</p>
+                            </p>
+                        </div>
+                    ))}
+                    <hr/>
+                    Total: <b>${getTotal()}</b>
+                    <hr/>
+                    {
+                        user ? (
+                            <button className="btn btn-sm btn-primary mt-2">
+                                Proceed to Checkout
+                            </button>
+                        ) : (
+                            <button className="btn btn-sm btn-primary mt-2">
+                                Login to Checkout
+                            </button>
+                        )
+                    }
+                </div>
+            </div>
+
+
+    </div>
+    );
+};
+
+export default Cart;
