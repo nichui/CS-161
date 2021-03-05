501b688d569d05bbf8f020074b42b0c14fb4cd86
Danh Nguyen <vn1007347@gmail.com>
2021-03-04 18:30:44 -0800

    Add Product to Local Storage and Tooltip

--- Ecommerce/client/src/components/cards/ProductCard.js
+++ Ecommerce/client/src/components/cards/ProductCard.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import {Card} from 'antd';
+import React, {useState} from 'react';
+import {Card, Tooltip} from 'antd';
 import {DeleteOutlined, EditOutlined, EyeOutlined, ShoppingCartOutlined} from "@ant-design/icons";
 import Default from '../../images/Default.jpg';
 import { Link } from "react-router-dom";
 import {showAverage} from "../../functions/rating";
+import _ from 'lodash';
 
 
 const {Meta} = Card;
 
 
 const ProductCard = ({product}) => {
+    const [tooltip, setTooltip] = useState('Click to add');
     //destructure
     const {images, title, description, slug, price} = product;
+
+    const handleAddToCart = () => {
+
+        // create cart array
+        let cart = []
+        if(typeof window !== 'undefined'){
+            // if cart is in localstorage GET it
+            if(localStorage.getItem('cart')){
+                cart = JSON.parse(localStorage.getItem('cart'));
+            }
+            // push new product to cart
+            cart.push({
+                ...product,
+                count: 1,
+            });
+            // remove duplicates
+            let unique = _.uniqWith(cart, _.isEqual)
+            // save to local storage
+            // console.log('unique', unique)
+            localStorage.setItem('cart', JSON.stringify(unique));
+            // show tooltip
+            setTooltip("Added");
+        }
+    }
+
+
     return(
     <>
         {product && product.ratings && product.ratings.length > 0
@@ -30,9 +58,11 @@ const ProductCard = ({product}) => {
             <Link to={`/product/${slug}`}>
                 <EyeOutlined className="text-warning"/> <br/> View Product
             </Link>,
-            <>
-                <ShoppingCartOutlined className="text-danger"/> <br/> Add to Cart
-            </>,
+            <Tooltip title={tooltip}>
+                <a onClick={handleAddToCart}>
+                    <ShoppingCartOutlined className="text-danger"/> <br/> Add to Cart
+                </a>
+            </Tooltip>
         ]}
         >
         <Meta
