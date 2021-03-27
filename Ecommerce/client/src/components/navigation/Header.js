ef5d3eaec9ba396c5e0bf99e54a545ceb683bac2
tchu1997 <tuong.chu@sjsu.edu>
2021-03-26 18:45:27 -0700

    App Logo

--- Ecommerce/client/src/components/navigation/Header.js
+++ Ecommerce/client/src/components/navigation/Header.js
@@ -53,15 +53,6 @@ const Header = () => {
     }
     return(
         
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-             {/* Icons for Navigation components*/}
-            <Item key="home">
-                <Link className="app-title" to="/">
-                    <img className="app-logo" src={Icons.Beans} />
-                    Got a Spot
-                </Link>
-            </Item>
-      
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" style={{ fontFamily: "Arial" }}>
             {/* Icons for Navigation components*/}
             {!user && (
@@ -82,8 +73,11 @@ const Header = () => {
             </Item> 
             */}
 
-            <Item key="home" className="float-left" style={styleText} icon={<HomeOutlined/>}>
-                <Link to="/">Home</Link> {/*Link to = "" equals to href ="" (go to another page)*/}
+            <Item key="home" className="float-left">
+                <Link className="app-title" to="/">
+                    <img className="app-logo" src={Icons.Beans} />
+                    Got a Spot
+                </Link>
             </Item>
 
             <Item key="shop" className="float-left" style={styleText} icon={<ShoppingOutlined/>}>
