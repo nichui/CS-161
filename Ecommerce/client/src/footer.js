cbe3b80fe18720643f29cec6239370a8336c78a0
Danh Nguyen <vn1007347@gmail.com>
2021-05-11 17:09:02 -0700

    Modify Footer

--- Ecommerce/client/src/footer.js
+++ Ecommerce/client/src/footer.js
@@ -5,34 +5,100 @@ import "./Footer.css"
 import {faHome, faMapMarked, faMarker, faMapMarker} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactMapGL, {Marker} from 'react-map-gl';
+import { faFacebook, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import {black} from "colorette";
 
 export default function Footer() {
     const [viewport, setViewport] = useState({
-        latitude: 37.306070,
-        longitude: -121.840899,
-        width: "20vw",
+        latitude: 37.33656309710032,
+        longitude: -121.88114561224849,
+        width: "25vw",
         height: "20vh",
-        zoom: 15
+        zoom: 13
     })
-    return <div>
-        <ReactMapGL
+    return <div className="footer">
+        <Container>
+            <Row>
+                <nav className="footer-nav">
+                    <Row>
+                        <ul className="iconUL">
+                            <li><FontAwesomeIcon className="iconFB" icon={faFacebook}/></li>
+                            <li><FontAwesomeIcon className="iconLin" icon={faLinkedinIn}/></li>
+                        </ul>
+                    </Row>
+                    <ul className="wordUL">
+                        <li>
+                            <a
+                                href="/"
 
-            {...viewport}
-            mapboxApiAccessToken={"pk.eyJ1IjoibmdvY29uY2VwdCIsImEiOiJja29pNzVxYmwwMWcxMnZvODcweDgxeGp2In0.oV71ZxSc2UNG5NEE2-wT1A"}
-            mapStyle="mapbox://styles/ngoconcept/ckoi9sdnu03dt18qlyl5r3mty"
-            onViewportChange={viewport => {
-                setViewport(viewport);
-            }}
-        >
-            <Marker
-                latitude={37.306070}
-                longitude={-121.840899}
-            >
+                            >
+                                <h4 className="link">GOT A SPOT</h4>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/About"
+                            >
+                                <h4 className="link">About Us</h4>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/FAQ"
 
-                <FontAwesomeIcon className="markerIcon" icon={faMapMarker}/>
+                            >
+                                <h4 className="link">FAQ</h4>
 
-            </Marker>
-        </ReactMapGL>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/register"
+
+                            >
+                                <h4 className="link">REGISTER</h4>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="credits ml-auto">
+
+
+                    <ReactMapGL
+                        className="mapBox"
+                        {...viewport}
+                        mapboxApiAccessToken={"pk.eyJ1IjoibmdvY29uY2VwdCIsImEiOiJja29pNzVxYmwwMWcxMnZvODcweDgxeGp2In0.oV71ZxSc2UNG5NEE2-wT1A"}
+                        mapStyle="mapbox://styles/ngoconcept/ckoi9sdnu03dt18qlyl5r3mty"
+                        onViewportChange={viewport => {
+                            setViewport(viewport);
+                        }}
+                    >
+                        <Marker
+                            latitude={37.33656309710032}
+                            longitude={-121.88114561224849}
+                        >
+
+                            <FontAwesomeIcon className="markerIcon" icon={faMapMarker}/>
+
+                        </Marker>
+                    </ReactMapGL>
+
+
+                </div>
+
+            </Row>
+
+            <Row>
+                <div >
+                    <span className="right">
+                        Copyright@  2021 All rights reserved | This template is made with by <a className="CoolBeansWord"  href="">Cool Beans</a>
+                    </span>
+                </div>
+            </Row>
+
+        </Container>
+
     </div>
 }
 
